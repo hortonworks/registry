b587e36913fc6c041ec8ab4ce356fae798dc39a0
Suresh Srinivas <sureshms@yahoo-inc.com>
2016-10-04 16:38:05 -0700

    Remove wildcard imports to adhere to coding standards

--- schema-registry/core/src/test/java/com/hortonworks/registries/schemaregistry/serde/SamplePullDeserializerTest.java
+++ schema-registry/core/src/test/java/com/hortonworks/registries/schemaregistry/serde/SamplePullDeserializerTest.java
@@ -26,7 +26,11 @@ import org.junit.Test;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 /**
  *
