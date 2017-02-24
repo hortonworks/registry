96163dbe6932c922eb1dafe2a34f3bb771038141
Sriharsha Chintalapani <harsha@hortonworks.com>
2017-02-23 21:28:37 -0800

    creating docs branch

--- schema-registry/core/src/main/resources/mysql/drop_schemaregistry-tables.sql
+++
@@ -1,6 +0,0 @@
--- USE schema_registry;
-DROP TABLE IF EXISTS schema_serdes_mapping;
-DROP TABLE IF EXISTS schema_serdes_info;
-DROP TABLE IF EXISTS schema_field_info;
-DROP TABLE IF EXISTS schema_version_info;
-DROP TABLE IF EXISTS schema_metadata_info;
