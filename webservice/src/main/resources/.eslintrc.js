d34de260ca4f7db197df1157e2174d6b87858f95
Jeffrey Ricker <ricker@rickerlyman.com>
2017-04-18 15:40:44 -0400

    removed changes to header

--- webservice/src/main/resources/.eslintrc.js
+++ webservice/src/main/resources/.eslintrc.js
@@ -30,7 +30,7 @@ module.exports = {
         "  * See the License for the specific language governing permissions and",
         "  * limitations under the License.",
         "*"
-     ]],
+    ]],
     "comma-dangle": [
       "error",
       "never"
