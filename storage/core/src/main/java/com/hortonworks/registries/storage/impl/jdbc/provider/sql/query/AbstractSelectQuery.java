81fe47a22c66bc294616924c9e7172fc8eab20ef
Satish Duggana <satishd@apache.org>
2017-04-26 16:31:05 +0530

    ISSUE-70: Added order-by support in JDBC StorageManager and added integration tests for MySql

--- storage/core/src/main/java/com/hortonworks/registries/storage/impl/jdbc/provider/sql/query/AbstractSelectQuery.java
+++ storage/core/src/main/java/com/hortonworks/registries/storage/impl/jdbc/provider/sql/query/AbstractSelectQuery.java
@@ -50,4 +50,28 @@ public abstract class AbstractSelectQuery extends AbstractStorableKeyQuery {
 
     protected abstract void addOrderByFieldsToParameterizedSql();
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        AbstractSelectQuery that = (AbstractSelectQuery) o;
+
+        return orderByFields != null ? orderByFields.equals(that.orderByFields) : that.orderByFields == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (orderByFields != null ? orderByFields.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractSelectQuery{" +
+                "orderByFields=" + orderByFields +
+                '}' + super.toString();
+    }
 }
