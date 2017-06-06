93827655218d0550bf91c37a515e839c50fbcb40
Priyank <pshah@hortonworks.com>
2017-06-06 16:26:59 -0700

    Minor: Remove the 401.html page filter bypass

--- common-auth/src/main/java/com/hortonworks/registries/auth/server/AuthenticationFilter.java
+++ common-auth/src/main/java/com/hortonworks/registries/auth/server/AuthenticationFilter.java
@@ -484,9 +484,6 @@ public class AuthenticationFilter implements Filter {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         boolean isHttps = "https".equals(httpRequest.getScheme());
-        String requestURI = ((HttpServletRequest) request).getRequestURI();
-        if (requestURI != null && requestURI.endsWith("401.html"))
-          doFilter(filterChain, httpRequest, httpResponse);
         try {
             boolean newToken = false;
             AuthenticationToken token;
