060c0e1fbf65e4a1c4cc2fe807eac36d11736173
Satish Duggana <satishd@apache.org>
2017-08-22 10:31:55 +0530

    Quick fix to get SQLScriptRunner to worker with existing sql scripts

--- bootstrap/sql/mysql/create_tables.sql
+++ bootstrap/sql/mysql/create_tables.sql
@@ -1,19 +1,19 @@
--- Copyright 2016 Hortonworks.
---
+-- Copyright 2016 Hortonworks.;
+-- ;
 -- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---    http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
-
--- THE NAMES OF THE TABLE COLUMNS MUST MATCH THE NAMES OF THE CORRESPONDING CLASS MODEL FIELDS
-
+-- you may not use this file except in compliance with the License.;
+-- You may obtain a copy of the License at;
+-- ;
+--    http://www.apache.org/licenses/LICENSE-2.0;
+-- ;
+-- Unless required by applicable law or agreed to in writing, software;
+-- distributed under the License is distributed on an "AS IS" BASIS,;
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.;
+-- See the License for the specific language governing permissions and;
+-- limitations under the License.;
+-- ;
+-- THE NAMES OF THE TABLE COLUMNS MUST MATCH THE NAMES OF THE CORRESPONDING CLASS MODEL FIELDS;
+-- ;
 -- USE schema_registry;
 
 CREATE TABLE IF NOT EXISTS schema_metadata_info (
