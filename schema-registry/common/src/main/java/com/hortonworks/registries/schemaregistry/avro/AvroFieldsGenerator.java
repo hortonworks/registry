8801b51bdf83f2361ec269fa98eff7755ccf7d1d
Wesley Lawrence <wesleyll505@gmail.com>
2018-03-14 09:55:34 -0400

    ISSUE-410 AvroFieldsGenerator produces Stackoverflow error when parsing a nested record schema. (#411)
    * ISSUE-410 AvroFieldsGenerator produces Stackoverflow error when parsing a nested record schema. #411 
    
    Added test resources for testing nested schemas.
    Added tests to make sure AvroFieldsGenerator and AvroSchemaProvider can parse nested schemas.
    Added code to AvroFieldsGenerator to handle parsing nested schemas.

--- schema-registry/common/src/main/java/com/hortonworks/registries/schemaregistry/avro/AvroFieldsGenerator.java
+++ schema-registry/common/src/main/java/com/hortonworks/registries/schemaregistry/avro/AvroFieldsGenerator.java
@@ -21,7 +21,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  *
@@ -46,13 +48,15 @@ public class AvroFieldsGenerator {
             LOG.debug("Schema full name: [{}]", fullName);
 
             List<Schema.Field> fields = schema.getFields();
+            Set<String> visitedRecords = new HashSet<>();
+            visitedRecords.add(schema.getFullName());
             for (Schema.Field field : fields) {
-                parseField(field, schemaFieldInfos);
+                parseField(field, schemaFieldInfos, visitedRecords);
             }
         }
     }
 
-    private void parseField(Schema.Field field, List<SchemaFieldInfo> schemaFieldInfos) {
+    private void parseField(Schema.Field field, List<SchemaFieldInfo> schemaFieldInfos, Set<String> visitedRecords) {
         Schema schema = field.schema();
         Schema.Type type = schema.getType();
         String name = field.name();
@@ -67,36 +71,44 @@ public class AvroFieldsGenerator {
         schemaFieldInfos.add(new SchemaFieldInfo(namespace, name, type.name()));
 
         // todo check whether fields should be mapped to the root schema.
-        parseSchema(schema, schemaFieldInfos);
+        parseSchema(schema, schemaFieldInfos, visitedRecords);
     }
 
-    private void parseSchema(Schema schema, List<SchemaFieldInfo> schemaFieldInfos) {
+    private void parseSchema(Schema schema, List<SchemaFieldInfo> schemaFieldInfos, Set<String> visitedRecords) {
         Schema.Type type = schema.getType();
         LOG.debug("Visiting type: [{}]", type);
 
         switch (type) {
             case RECORD:
-                // store fields of a record.
-                List<Schema.Field> fields = schema.getFields();
-                for (Schema.Field recordField : fields) {
-                    parseField(recordField, schemaFieldInfos);
+
+                String completeName = schema.getFullName();
+
+                // Since we are only interested in primitive data types, if we encounter a record that was already parsed it can be ignored
+                if (!visitedRecords.contains(completeName)) {
+                    visitedRecords.add(completeName);
+
+                    // store fields of a record.
+                    List<Schema.Field> fields = schema.getFields();
+                    for (Schema.Field recordField : fields) {
+                        parseField(recordField, schemaFieldInfos, visitedRecords);
+                    }
                 }
                 break;
             case MAP:
                 Schema valueTypeSchema = schema.getValueType();
-                parseSchema(valueTypeSchema, schemaFieldInfos);
+                parseSchema(valueTypeSchema, schemaFieldInfos, visitedRecords);
                 break;
             case ENUM:
                 break;
             case ARRAY:
                 Schema elementType = schema.getElementType();
-                parseSchema(elementType, schemaFieldInfos);
+                parseSchema(elementType, schemaFieldInfos, visitedRecords);
                 break;
 
             case UNION:
                 List<Schema> unionTypes = schema.getTypes();
                 for (Schema typeSchema : unionTypes) {
-                    parseSchema(typeSchema, schemaFieldInfos);
+                    parseSchema(typeSchema, schemaFieldInfos, visitedRecords);
                 }
                 break;
 
