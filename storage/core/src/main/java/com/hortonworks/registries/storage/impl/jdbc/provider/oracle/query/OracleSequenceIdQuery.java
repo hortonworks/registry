ba7adb02c6cb0353e659e17c562460d94f2366b0
Saravanan Raju <sraju@hortonworks.com>
2019-06-24 23:17:47 +0530

    HOTFIX : Close unclosed ResultSet in storage (#559)

--- storage/core/src/main/java/com/hortonworks/registries/storage/impl/jdbc/provider/oracle/query/OracleSequenceIdQuery.java
+++ storage/core/src/main/java/com/hortonworks/registries/storage/impl/jdbc/provider/oracle/query/OracleSequenceIdQuery.java
@@ -17,15 +17,14 @@
 package com.hortonworks.registries.storage.impl.jdbc.provider.oracle.query;
 
 import com.hortonworks.registries.storage.impl.jdbc.config.ExecutionConfig;
-import com.hortonworks.registries.storage.impl.jdbc.connection.ConnectionBuilder;
 import com.hortonworks.registries.storage.impl.jdbc.provider.oracle.statement.OracleDataTypeContext;
 import com.hortonworks.registries.storage.impl.jdbc.provider.sql.query.AbstractSqlQuery;
 import com.hortonworks.registries.storage.impl.jdbc.provider.sql.statement.PreparedStatementBuilder;
-import com.hortonworks.registries.storage.transaction.TransactionBookKeeper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -47,8 +46,8 @@ public class OracleSequenceIdQuery {
         OracleSqlQuery nextValueQuery = new OracleSqlQuery(String.format("SELECT \"%s\".%s from DUAL", namespace.toUpperCase(), nextValueFunction));
         Long nextId = 0l;
 
-        try {
-            ResultSet selectResultSet = PreparedStatementBuilder.of(connection, new ExecutionConfig(queryTimeoutSecs), oracleDatabaseStorageContext, nextValueQuery).getPreparedStatement(nextValueQuery).executeQuery();
+        try (PreparedStatement preparedStatement = PreparedStatementBuilder.of(connection, new ExecutionConfig(queryTimeoutSecs), oracleDatabaseStorageContext, nextValueQuery).getPreparedStatement(nextValueQuery);
+             ResultSet selectResultSet = preparedStatement.executeQuery()) {
             if (selectResultSet.next()) {
                 nextId = selectResultSet.getLong(nextValueFunction);
             } else {
