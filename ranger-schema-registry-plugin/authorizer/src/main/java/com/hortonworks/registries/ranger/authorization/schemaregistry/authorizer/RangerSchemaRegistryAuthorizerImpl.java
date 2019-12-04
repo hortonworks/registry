d7d9553eecb09b238c4d28738aba58bdf5b99f7c
vomoshkovskyi <vomoshkovskyi@cloudera.com>
2019-12-05 01:48:17 +0200

    UserGroupInformation class should be loaded by parent classloader, otherwise UGI will be re-initialized with wrong parameters

--- ranger-schema-registry-plugin/authorizer/src/main/java/com/hortonworks/registries/ranger/authorization/schemaregistry/authorizer/RangerSchemaRegistryAuthorizerImpl.java
+++ ranger-schema-registry-plugin/authorizer/src/main/java/com/hortonworks/registries/ranger/authorization/schemaregistry/authorizer/RangerSchemaRegistryAuthorizerImpl.java
@@ -5,7 +5,6 @@ import org.apache.ranger.plugin.policyengine.RangerAccessResourceImpl;
 import org.apache.ranger.plugin.policyengine.RangerAccessResult;
 import org.apache.ranger.plugin.service.RangerBasePlugin;
 
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
