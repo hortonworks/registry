98fa7c0ebfd4345e5b0a0d6dd2cc86a91222296b
Andras Katona <akatona@cloudera.com>
2020-04-22 14:57:13 +0200

    [CDPD-10560] Using CDP version of Ranger
    Change-Id: I1094caa94109123882c76dcf4d86fe0730ba2d2e

--- schema-registry/authorizer/ranger-plugin/ranger-authorizer/src/test/java/com/hortonworks/registries/schemaregistry/authorizer/ranger/util/TestRangerAdminClientImpl.java
+++ schema-registry/authorizer/ranger-plugin/ranger-authorizer/src/test/java/com/hortonworks/registries/schemaregistry/authorizer/ranger/util/TestRangerAdminClientImpl.java
@@ -35,17 +35,6 @@ public class TestRangerAdminClientImpl extends AbstractRangerAdminClient {
     private static final Logger LOG = LoggerFactory.getLogger(TestRangerAdminClientImpl.class);
     private final static String cacheFilename = "schema-registry-policies.json";
     private final static String tagFilename = "schema-registry-policies-tag.json";
-    private Gson gson;
-
-    public void init(String serviceName, String appId, String configPropertyPrefix) {
-        Gson gson = null;
-        try {
-            gson = new GsonBuilder().setDateFormat("yyyyMMdd-HH:mm:ss.SSS-Z").setPrettyPrinting().create();
-        } catch(Throwable excp) {
-            LOG.error("TestRangerAdminClientImpl: failed to create GsonBuilder object", excp);
-        }
-        this.gson = gson;
-    }
 
     public ServicePolicies getServicePoliciesIfUpdated(long lastKnownVersion, long lastActivationTimeInMillis) throws Exception {
 
