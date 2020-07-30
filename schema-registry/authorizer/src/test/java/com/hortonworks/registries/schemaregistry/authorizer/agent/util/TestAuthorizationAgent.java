8675b55839bb2bfe01deca1a5d6807a3ca9f92e1
galyo <galyo@cloudera.com>
2020-07-30 12:33:58 +0200

    [tech debt] CDPD-15633 Remove sun.reflect classes
    Change-Id: I6668ba92948835225f1a49d0252e86e381aa7666

--- schema-registry/authorizer/src/test/java/com/hortonworks/registries/schemaregistry/authorizer/agent/util/TestAuthorizationAgent.java
+++ schema-registry/authorizer/src/test/java/com/hortonworks/registries/schemaregistry/authorizer/agent/util/TestAuthorizationAgent.java
@@ -27,7 +27,6 @@ import com.hortonworks.registries.schemaregistry.authorizer.agent.AuthorizationA
 import com.hortonworks.registries.schemaregistry.authorizer.core.Authorizer;
 import com.hortonworks.registries.schemaregistry.authorizer.exception.AuthorizationException;
 import com.hortonworks.registries.schemaregistry.errors.SchemaNotFoundException;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Collection;
 import java.util.Map;
@@ -39,46 +38,46 @@ public class TestAuthorizationAgent implements AuthorizationAgent {
     @Override
     public Collection<AggregatedSchemaMetadataInfo> authorizeGetAggregatedSchemaList
             (Authorizer.UserAndGroups userAndGroups, Collection<AggregatedSchemaMetadataInfo> aggregatedSchemaMetadataInfoList) {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
     public AggregatedSchemaMetadataInfo authorizeGetAggregatedSchemaInfo
             (Authorizer.UserAndGroups userAndGroups, 
              AggregatedSchemaMetadataInfo aggregatedSchemaMetadataInfo) {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
     public Collection<SchemaMetadataInfo> authorizeFindSchemas(Authorizer.UserAndGroups userAndGroups,
                                                                Collection<SchemaMetadataInfo> schemas) {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
     public Collection<SchemaVersionKey> authorizeFindSchemasByFields(Authorizer.UserAndGroups userAndGroups,
                                                                      ISchemaRegistry schemaRegistry,
                                                                      Collection<SchemaVersionKey> versions) {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
     public void authorizeDeleteSchemaMetadata(Authorizer.UserAndGroups userAndGroups, ISchemaRegistry schemaRegistry, String schemaMetadataName) throws AuthorizationException, SchemaNotFoundException {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
     public void authorizeSchemaMetadata(Authorizer.UserAndGroups userAndGroups,
                                         SchemaMetadata schemaMetadata,
                                         Authorizer.AccessType accessType)   {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
     public void authorizeSchemaMetadata(Authorizer.UserAndGroups userAndGroups,
                                         SchemaMetadataInfo schemaMetadataInfo,
                                         Authorizer.AccessType accessType)   {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
@@ -86,7 +85,7 @@ public class TestAuthorizationAgent implements AuthorizationAgent {
                                         ISchemaRegistry schemaRegistry,
                                         String schemaMetadataName,
                                         Authorizer.AccessType accessType)   {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
@@ -95,14 +94,14 @@ public class TestAuthorizationAgent implements AuthorizationAgent {
                                             String schemaMetadataName,
                                             Long versionId,
                                             String branchTocreate)   {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
     public void authorizeDeleteSchemaBranch(Authorizer.UserAndGroups userAndGroups,
                                             ISchemaRegistry schemaRegistry,
                                             Long schemaBranchId)   {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
@@ -110,7 +109,7 @@ public class TestAuthorizationAgent implements AuthorizationAgent {
                                                             ISchemaRegistry schemaRegistry,
                                                             String schemaMetadataName,
                                                             Collection<SchemaBranch> branches)  {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
@@ -119,7 +118,7 @@ public class TestAuthorizationAgent implements AuthorizationAgent {
                                        String schemaMetadataName,
                                        String schemaBranch,
                                        Authorizer.AccessType accessType)   {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
@@ -127,7 +126,7 @@ public class TestAuthorizationAgent implements AuthorizationAgent {
                                        ISchemaRegistry schemaRegistry,
                                        SchemaVersionKey versionKey,
                                        Authorizer.AccessType accessType) {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
@@ -135,7 +134,7 @@ public class TestAuthorizationAgent implements AuthorizationAgent {
                                        ISchemaRegistry schemaRegistry,
                                        SchemaVersionInfo versionInfo,
                                        Authorizer.AccessType accessType) {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
@@ -143,7 +142,7 @@ public class TestAuthorizationAgent implements AuthorizationAgent {
                                        ISchemaRegistry schemaRegistry,
                                        SchemaIdVersion versionId,
                                        Authorizer.AccessType accessType) {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
@@ -151,40 +150,40 @@ public class TestAuthorizationAgent implements AuthorizationAgent {
                                        ISchemaRegistry schemaRegistry,
                                        Long versionId,
                                        Authorizer.AccessType accessType) {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
     public void authorizeMergeSchemaVersion(Authorizer.UserAndGroups userAndGroups,
                                             ISchemaRegistry schemaRegistry,
                                             Long versionId) {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
     public void authorizeSerDes(Authorizer.UserAndGroups userAndGroups,
                                 Authorizer.AccessType accessType)   {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
     public void authorizeGetSerializers(Authorizer.UserAndGroups userAndGroups,
                                         SchemaMetadataInfo schemaMetadataInfo)   {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
     public void authorizeMapSchemaWithSerDes(Authorizer.UserAndGroups userAndGroups,
                                              ISchemaRegistry schemaRegistry,
                                              String schemaMetadataName)   {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
     @Override
     public Collection<SchemaVersionInfo> authorizeGetAllVersions(Authorizer.UserAndGroups userAndGroups,
                                                              ISchemaRegistry schemaRegistry,
                                                                  Collection<SchemaVersionInfo> versions) {
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented");
     }
 
 }
