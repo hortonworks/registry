1703b6c556720a0b0d16e209499cf49d26740544
Andras Katona <akatona@cloudera.com>
2021-02-02 13:27:35 +0100

    [ci] Checkstyle results collector
    Change-Id: I02a33e4e937e847a87c15bc6bf5f4a221a7d6900

--- ci/collect_checkstyle_results.py
+++ ci/collect_checkstyle_results.py
@@ -5,28 +5,31 @@ import xml.etree.ElementTree as ET
 
 print('<results>')
 abspath = os.path.abspath('.')
-checkstyle_result = 'checkstyle-result.xml'
+checkstyle_results = ['main.xml', 'test.xml']
 for root, directories, filenames in os.walk('.'):
   target_dir = '/build/reports/checkstyle'
-  if root.endswith(target_dir) and checkstyle_result in filenames:
+  if root.endswith(target_dir) and any(x in filenames for x in checkstyle_results):
     module = root[2: len(root) - len(target_dir)]
-    file_path = os.path.join(root, checkstyle_result)
-    xml = ET.parse(file_path)
-    checkstyle = xml.getroot()
-    if checkstyle.find('file/error') is not None:
-      print('<module>')
-      print('<name>{}</name>'.format(module))
-      for elem in checkstyle.findall('file'):
-        file_name = elem.get('name').replace(abspath + '/', '')
-        errors = elem.findall('error')
-        if len(errors) > 0:
-          print('<file>')
-          print('<name>{}</name>'.format(file_name))
-          for error in errors:
-            print('<error>')
-            print('<src>{}:{}</src>'.format(os.path.basename(file_name), error.get('line')))
-            print('<message>{}</message>'.format(error.get('message')))
-            print('</error>')
-          print('</file>')
-      print('</module>')
+    for checkstyle_result in checkstyle_results:
+      file_path = os.path.join(root, checkstyle_result)
+      if not os.path.exists(file_path):
+        continue
+      xml = ET.parse(file_path)
+      checkstyle = xml.getroot()
+      if checkstyle.find('file/error') is not None:
+        print('<module>')
+        print('<name>{}</name>'.format(module + ' - ' + checkstyle_result))
+        for elem in checkstyle.findall('file'):
+          file_name = elem.get('name').replace(abspath + '/', '')
+          errors = elem.findall('error')
+          if len(errors) > 0:
+            print('<file>')
+            print('<name>{}</name>'.format(file_name))
+            for error in errors:
+              print('<error>')
+              print('<src>{}:{}</src>'.format(os.path.basename(file_name), error.get('line')))
+              print('<message>{}</message>'.format(error.get('message')))
+              print('</error>')
+            print('</file>')
+        print('</module>')
 print('</results>')
