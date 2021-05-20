6abada51dfb092a7fd25f43a4ff61fb3d4674eb2
Alan Artigao Carreño <alanartigao@gmail.com>
2021-05-20 15:01:58 +0200

    Fix fingerprint method for null default values (#733)
    * Fix default toString for JsonProperties.NULL_VALUE
    
    The normalization of the Schema is inconsistent across restarts due to object's JsonProperties.NULL_VALUE lack of override toString.
    
    This fixes that and allows deterministic creation for fingerprints.
    
    * Assert that fingerprint hash remains the same across executions
    
    Added test case to assess that generated fingerprint is always the same
    between multiple executions, to prevent using an invalid stringified
    version of Object.
    
    Same Schema string must return same fingerprint, at least as long as
    normalize function is not changed. Forced MD5, just in case in future
    releases, the default hashing algorithm changes.
    
    Change-Id: I8b197b422f60873b50f419bdec7e93d5f187e8cb

---
+++ schema-registry/schema-registry-common/src/test/java/com/hortonworks/registries/schemaregistry/avro/AvroSchemaProviderTest.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2016-2021 Cloudera, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package com.hortonworks.registries.schemaregistry.avro;
+
+import com.hortonworks.registries.schemaregistry.SchemaProvider;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class AvroSchemaProviderTest {
+
+    private String schemaWithNullDefaults = "{\n" +
+            "  \"type\" : \"record\",\n" +
+            "  \"namespace\" : \"com.hortonworks.registries\",\n" +
+            "  \"name\" : \"trucks\",\n" +
+            "  \"fields\" : [\n" +
+            "    { \"name\" : \"driverId\" , \"type\" : [\"null\", \"int\"], \"default\": null }" +
+            "  ]\n" +
+            "}\n";
+
+    @Test
+    public void testGetFingerprintNullTypeDeterminism() throws Exception {
+        AvroSchemaProvider avroSchemaProvider = new AvroSchemaProvider();
+        avroSchemaProvider.init(Collections.singletonMap(SchemaProvider.HASH_FUNCTION_CONFIG, "MD5"));
+
+        String withNullDefaultsFingerprintHex = bytesToHex(avroSchemaProvider.getFingerprint(schemaWithNullDefaults));
+
+        Assert.assertThat(withNullDefaultsFingerprintHex, equalTo("e1e17a3aef8c728c131204bbf49046b2"));
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+}
