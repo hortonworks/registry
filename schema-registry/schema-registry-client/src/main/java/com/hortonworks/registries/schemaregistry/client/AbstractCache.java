9d92dc2e61fc02cd35269b9275eb1487c952ac97
Imre Balassa <ibalassa@cloudera.com>
2023-02-22 22:28:17 +0100

    CDPD-49482: ISSUE-798 SR client SchemaVersionInfoCache is not thread safe
       "This commit does not contain secrets"
    
    Change-Id: I1fa93e6ae3bcdf0432fbf7280ed3b2230d50f919

--- schema-registry/schema-registry-client/src/main/java/com/hortonworks/registries/schemaregistry/client/AbstractCache.java
+++ schema-registry/schema-registry-client/src/main/java/com/hortonworks/registries/schemaregistry/client/AbstractCache.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018-2019 Cloudera, Inc.
+ * Copyright 2018-2023 Cloudera, Inc.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,9 @@
  * limitations under the License.
  **/
 
-package com.hortonworks.registries.schemaregistry.cache;
+package com.hortonworks.registries.schemaregistry.client;
+
+import com.hortonworks.registries.schemaregistry.cache.SchemaRegistryCacheType;
 
 public interface AbstractCache {
 
